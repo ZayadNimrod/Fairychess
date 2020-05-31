ec24fb2e6999a7eccd4a74a270cdcad54fd10eb7
ZayadNimrod <32575659+ZayadNimrod@users.noreply.github.com>
2020-05-31 20:59:21 +0100

    Fixed misalignment issues; big issue was floating point drift; fixed by only scaling at the end

--- src/main/scala/Visualiser.scala
+++ src/main/scala/Visualiser.scala
@@ -7,17 +7,17 @@ import doodle.java2d._
 
 object Visualiser {
 
-  val scale = 50
-  val thickness = 5
+  val scale: Double = 50
+  val thickness: Double = 5
 
-  val hyp = Math.sqrt(Math.pow(scale,2)*2)
+  val hyp = Math.sqrt(Math.pow(scale, 2) * 2)
 
-  val blackSquare: Image = Image.rectangle(scale, scale).fillColor(Color.black)
-  val whiteSquare: Image = Image.rectangle(scale, scale).fillColor(Color.rgb(222, 222, 222))
-  val clearSquare: Image = Image.rectangle(scale, scale).fillColor(Color.rgba(0, 0, 0, 0)).strokeColor((Color.rgba(0, 0, 0, 0)))
-  val startSquare: Image = Image.star(5, scale/ 2.0, scale / 4.0, Angle(-195.toRadians)).strokeColor(Color.red).strokeWidth(thickness)
-  val finalSquare: Image = Image.star(5, scale / 2.0, scale / 4.0, Angle(-195.toRadians)).strokeColor(Color.green).strokeWidth(thickness)
-  val interSquare: Image = Image.circle(scale ).strokeColor(Color.lightBlue).strokeWidth(thickness)
+  val blackSquare: Image = Image.rectangle(1, 1).fillColor(Color.black).strokeWidth(0)
+  val whiteSquare: Image = Image.rectangle(1, 1).fillColor(Color.rgb(222, 222, 222)).strokeWidth(0)
+  val clearSquare: Image = Image.rectangle(1, 1).fillColor(Color.rgba(0, 0, 0, 0)).strokeColor((Color.rgba(0, 0, 0, 0))).strokeWidth(0)
+  val startSquare: Image = Image.star(4, 1 / 2.0, 1 / 4.0, Angle(-0.toRadians)).strokeColor(Color.red).strokeWidth(thickness / scale)
+  val finalSquare: Image = Image.star(4, 1 / 2.0, 1 / 4.0, Angle(-0.toRadians)).strokeColor(Color.green).strokeWidth(thickness / scale)
+  val interSquare: Image = Image.circle(1).strokeColor(Color.lightBlue).strokeWidth(thickness / scale).at(-0.5, -0.5)
 
   def Show(piece: String, x: Int, y: Int): Unit = {
     PieceCompiler(piece).toOption.map(_ => DrawMoves(_, x, y))
@@ -27,16 +27,16 @@ object Visualiser {
 
   def DrawMoves(moves: ChoiceOfSequence, x: Int, y: Int) = {
 
-    val centerX: Int = x / 2
-    val centerY: Int = y / 2
+    val centerX: Int = x-Math.floorDiv(x, 2)
+    val centerY: Int = y-Math.floorDiv(y, 2)
 
 
     //create the board
     val board: Image = Col(Row(blackSquare, whiteSquare, x), Row(whiteSquare, blackSquare, x), y)
 
-    val paths: Image = DrawPaths(moves, -centerX, -centerY, x - centerX - 1, y - centerY - 1)
+    val paths: Image = DrawPaths(moves, -centerX+1, -centerY+1, x - centerX , y - centerY )
     //offset moves then impose on board
-    val imposed: Image = board.under(paths.at((centerX - x / 2.0+0.5) * scale, (centerY - y / 2.0+0.5) * scale))
+    val imposed: Image = board.under(paths.at((centerX-x / 2.0), (centerY-y / 2.0 ))).scale(scale, scale)
 
     imposed.draw()
 
@@ -74,11 +74,11 @@ object Visualiser {
       } else {
         val subsequent = move.tail
 
-        val line = Image.line(atom.x * scale, atom.y * scale).strokeColor(Color.aliceBlue).strokeWidth(thickness).at((currentX + atom.x / 2.0) * scale, (currentY + atom.y / 2.0) * scale)
+        val line = Image.line(atom.x, atom.y).strokeColor(Color.aliceBlue).strokeWidth(thickness / scale).at((currentX + atom.x / 2.0), (currentY + atom.y / 2.0))
 
         val restOfMove = DrawPath(subsequent, minX, minY, maxX, maxY, currentX + atom.x, currentY + atom.y)
         val endSymbol = if (subsequent.isEmpty) finalSquare else interSquare
-        val lineWithConnectors = line.on(clearSquare.at(currentX * scale, currentY * scale)).on(endSymbol.at((currentX + atom.x) * scale, (currentY + atom.y) * scale))
+        val lineWithConnectors = line.on(clearSquare.at(currentX, currentY)).on(endSymbol.at((currentX + atom.x), (currentY + atom.y)))
         restOfMove.map(lineWithConnectors.on)
       }
     }
